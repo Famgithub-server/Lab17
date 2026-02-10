c22c3c0fe9a0c0c11e77e8cde294aa3e5a42a845
Famgithub-server <fam0637186002@gmail.com>
2026-02-10 07:55:36 +0700

    update

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -4,36 +4,36 @@
 #include<vector>
 using namespace std;
 
-______________ randomVector(unsigned int N){	
+vector<int> randomVector(unsigned int N){    
     vector<int> v;
-    for(unsigned int i = 0; i < N; i++) v.______________(rand()%10);
-    ______________;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
 }
 
-______________ showVector(______________){
-	cout << "[";
-	for(unsigned int i = 0; _____________________; i++){
-		cout << _________________;
-		if(_________________) cout << "]";
-		else cout << " ";
-	}
+void showVector(vector<int> v){
+    cout << "[";
+    for(unsigned int i = 0; i<v.size(); i++){
+        cout << v[i];
+        if(i+1 == v.size()) cout << "]";
+        else cout << " ";
+    }
 }
 
-_________________ dotProduct(_____________________________){
-	int sum = 0;
-	for(unsigned int i = 0; _____________________; i++) sum += _________________;	
-	_________________;
+int dotProduct(vector<int> vx, vector<int> vy){
+    int sum = 0;
+    for(unsigned int i = 0; i<vx.size(); i++) sum += (vx[i] * vy[i]);   
+    return sum;
 }
 
 int main(){
-	srand(time(0));
-	vector<int> x = randomVector(5);
-	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
-	showVector(x);
-	cout << " . ";
-	showVector(y);
-	cout << " = " << z;
-	
-	return 0;
+    srand(time(0));
+    vector<int> x = randomVector(5);
+    vector<int> y = randomVector(5);
+    int z = dotProduct(x,y);
+    showVector(x);
+    cout << " . ";
+    showVector(y);
+    cout << " = " << z;
+    
+    return 0;
 }
